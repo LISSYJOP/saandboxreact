0a651cc891bdad796a5e3ea9b38fef37993413d8
Adriela_Hdz <adriela_hdz@outlook.com>
2021-04-19 12:04:00 -0600

    uso de props en la v3 de nuevocliente3

--- src/Paginas/Jsx/Cliente/VerClientes1.js
+++ src/Paginas/Jsx/Cliente/VerClientes1.js
@@ -4,6 +4,7 @@ siempre debe heredar de la clase component */
 import React, {Component} from 'react';
 import NuevoCliente1 from './NuevoCliente1';
 import NuevoCliente2 from './NuevoCliente2';
+import NuevoCliente3 from './NuevoCliente3';
 
 
 class verclientes1 extends Component{
@@ -16,6 +17,7 @@ class verclientes1 extends Component{
                <p>Uso de class based components</p>
                <NuevoCliente1/>
                <NuevoCliente2/>
+               <NuevoCliente3 nombre='Luke SkyWalker' edad='25'/>
            </div> 
         )
 
